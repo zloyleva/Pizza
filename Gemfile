5d8c3e4217de3258eabd66862d075e9c133d52be
Levchenko Oleg <zloyleva@gmail.com>
2016-05-17 16:25:35 +0300

    Add rekefile, update Gemfile

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gem "sqlite3"
 gem "activerecord"
 gem "sinatra-activerecord"
 gem "sinatra-contrib"
+
+group :development do
+	gem "tux"
+end
--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
